dd55c16ea9a1a48315a89bef1f73a73180177f5c
Deepanshu Jain <31172305+deepanshu-jain1999@users.noreply.github.com>
2025-05-30 14:59:32 +0530

    on deployment

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! you made it, testing automation'
+    return 'Hello, world! you made it, on deplyment'
 
 if __name__ == '__main__':
     app.run()
